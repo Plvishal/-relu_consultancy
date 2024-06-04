aea46ca0c33ffbdd73427ce29de5e3a522b3a55d
Vishal <vishalpal1501@gmail.com>
2024-06-05 00:57:12 +0530

    main card done

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,12 @@ function App() {
     <>
       <div className="flex flex-col  bg-[#F5F5F5]">
         <div className="flex w-full gap-2 px-2 sticky top-1 opacity-1 z-10">
-          <div className="w-[20%] h-[80px] bg-white shadow-md rounded-xl flex justify-center items-center">
+          <div className="w-[22%] h-[80px] bg-white shadow-md rounded-xl flex justify-center items-center">
             <h1 className="text-[2rem] font-bold font-sans uppercase bg-gradient-to-t from-red-300 via-pink-500 to-blue-400 text-transparent bg-clip-text">
               Logo
             </h1>
           </div>
-          <div className="w-[60%] h-[80px] bg-white shadow-md rounded-2xl flex justify-between items-center px-6">
+          <div className="w-[55%] h-[80px] bg-white shadow-md rounded-2xl flex justify-between items-center px-6">
             <div className="flex gap-2">
               <img
                 src={search}
@@ -37,18 +37,18 @@ function App() {
               <spna>Filters</spna>
             </div>
           </div>
-          <div className="w-[20%] h-[80px] shadow-md rounded-2xl bg-[#88C2BB] flex justify-center items-center">
+          <div className="w-[23%] h-[80px] shadow-md rounded-2xl bg-[#88C2BB] flex justify-center items-center">
             <span className="text-white font-semibold">Become a Seller</span>
           </div>
         </div>
         <div className="flex  md:flex-row flex-col justify-between w-full gap-2 p-2  ">
-          <div className="md:w-[20%]  p-2 md:h-[572px] md:sticky  relative left-0 md:top-32 md:mt-0 mt-48  shadow-2xl rounded-2xl bg-white ">
+          <div className="md:w-[22%]  p-2 md:h-[572px] md:sticky  relative left-0 md:top-32 md:mt-0 mt-48  shadow-2xl rounded-2xl bg-white ">
             <Sidebar />
           </div>
-          <div className="md:w-[60%] relative md:mt-9 mt-2 shadow-2xl rounded-xl bg-white opacity-1">
+          <div className="md:w-[55%] relative md:mt-9 mt-2 opacity-1">
             <ContentComponents />
           </div>
-          <div className="md:w-[20%] w-full md:h-[572px] p-2  md:sticky  right-0 md:top-32 top-2 shadow-2xl rounded-2xl overflow-scroll">
+          <div className="md:w-[23%] w-full md:h-[572px] p-2  md:sticky  right-0 md:top-32 top-2 shadow-2xl rounded-2xl overflow-scroll">
             <Artistic />
           </div>
         </div>
---
+++ utils/mainCarData.js
@@ -0,0 +1,20 @@
+import { cover1L, cover2l, coverIcon2, icon1 } from './imageExport';
+
+export const cardData = [
+  {
+    icon: icon1,
+    name: 'Lara Leones',
+    subname: '@thewallart',
+    description:
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+    image: cover1L,
+  },
+  {
+    icon: coverIcon2,
+    name: 'Thomas J.',
+    subname: '@thecustomcreater',
+    description:
+      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.',
+    image: cover2l,
+  },
+];

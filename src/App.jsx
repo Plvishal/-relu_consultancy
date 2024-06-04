d1f4c75adfedf6830b3ab59f0a157142a6d48ff9
Vishal <vishalpal1501@gmail.com>
2024-06-04 09:16:44 +0530

    page layout design done

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,78 @@ import './App.css';
 function App() {
   return (
     <>
-      <h1>Vite + React</h1>
+      <div className="flex flex-col  bg-[#F5F5F5]">
+        <div className="flex w-full gap-2 px-2 sticky top-1 opacity-1 z-10">
+          <div className="w-[20%] h-[80px] bg-white shadow-md rounded-xl">
+            Logo
+          </div>
+          <div className="w-[60%] h-[80px] bg-white shadow-md rounded-2xl">
+            Search
+          </div>
+          <div className="w-[20%] h-[80px] bg-white shadow-md rounded-2xl">
+            Bets Seller
+          </div>
+        </div>
+        <div className="flex  md:flex-row flex-col justify-between w-full gap-2 p-2  ">
+          <div className="md:w-[20%]  p-2 md:h-[572px] md:sticky  relative left-0 md:top-32 md:mt-0 mt-48  shadow-2xl rounded-2xl bg-white ">
+            sidebar
+          </div>
+          <div className="md:w-[60%] relative md:mt-9 mt-2 shadow-2xl rounded-xl bg-white opacity-1">
+            content
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+            <p>sfjhfhrefj</p>
+          </div>
+          <div className="md:w-[20%] w-full md:h-[572px] p-2  md:sticky  right-0 md:top-32 top-2 shadow-2xl rounded-2xl">
+            Artitic
+          </div>
+        </div>
+      </div>
     </>
   );
 }

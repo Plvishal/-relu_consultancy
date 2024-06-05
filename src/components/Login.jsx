4068b302f2afc1cc93b38edd1eee42b30d9ee1af
Vishal <vishalpal1501@gmail.com>
2024-06-05 09:48:25 +0530

    project done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { auth } from '../../config/firebase.js';
 import { useNavigate } from 'react-router';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -21,22 +22,51 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Login</h2>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-        />
-        <button type="submit">Login</button>
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-white">
+      <form
+        onSubmit={handleLogin}
+        className="flex  flex-col justify-center items-center  shadow-2xl p-7 gap-6 w-1/4 rounded-2xl"
+      >
+        <h2 className="mb-10 text-xl font-bold font-serif">Login Form</h2>
+
+        <div className="flex flex-col gap-2 w-full">
+          <label htmlFor="Email" className="text-lg fonr-bold font-serif">
+            Email ID
+          </label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="outline-none border-b-2 text-md text-neutral-400 font-semibold"
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+          <label htmlFor="Password" className="text-lg fonr-bold font-serif">
+            Password
+          </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="outline-none border-b-2 text-md text-neutral-400 font-semibold"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-md text-white font-snas font-semibold bg-blue-500 p-2 px-5 rounded-lg"
+        >
+          Login
+        </button>
+        <p>
+          If you don&apos;t have already account
+          <Link to="/" className="underline text-md font-bold">
+            Register
+          </Link>
+        </p>
       </form>
       {error && <p>{error}</p>}
     </div>
--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import { collection, addDoc } from 'firebase/firestore';
 import { auth, db } from '../../config/firebase.js';
+import { Link } from 'react-router-dom';
 function Registration() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
@@ -27,31 +28,63 @@ function Registration() {
     }
   };
   return (
-    <div>
-      <h2>Signup</h2>
-      <form onSubmit={handleSignup}>
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Username"
-          required
-        />
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Email"
-          required
-        />
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Password"
-          required
-        />
-        <button type="submit">Signup</button>
+    <div className="w-full h-screen flex flex-col justify-center items-center bg-white">
+      <form
+        onSubmit={handleSignup}
+        className="flex  flex-col justify-center items-center  shadow-2xl p-7 gap-6 w-1/4 rounded-2xl"
+      >
+        <h2 className="mb-10 text-xl font-bold font-serif">Signup Form</h2>
+        <div className="flex flex-col gap-2 w-full">
+          <label htmlFor="Username" className="text-lg fonr-bold font-serif">
+            Username
+          </label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Username"
+            className="outline-none border-b-2 text-md text-neutral-400 font-semibold"
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+          <label htmlFor="Email" className="text-lg fonr-bold font-serif">
+            Email ID
+          </label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Email"
+            className="outline-none border-b-2 text-md text-neutral-400 font-semibold"
+            required
+          />
+        </div>
+        <div className="flex flex-col gap-2 w-full">
+          <label htmlFor="Password" className="text-lg fonr-bold font-serif">
+            Password
+          </label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
+            className="outline-none border-b-2 text-md text-neutral-400 font-semibold"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="text-md text-white font-snas font-semibold bg-blue-500 p-2 px-5 rounded-lg"
+        >
+          Signup
+        </button>
+        <p>
+          If you have already account{' '}
+          <Link to="/login" className="underline text-md font-bold">
+            Login
+          </Link>
+        </p>
       </form>
       {error && <p>{error}</p>}
     </div>

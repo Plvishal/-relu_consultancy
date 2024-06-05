ebc25384e82d5e0650fd8c430892325aeddbaa9d
Vishal <vishalpal1501@gmail.com>
2024-06-05 09:21:53 +0530

    all logic done related to authentication

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,46 @@
-function Login() {
-  return <div>Login</div>;
-}
+import { useState } from 'react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../../config/firebase.js';
+import { useNavigate } from 'react-router';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      alert('Logged in successfully!');
+      navigate('/home');
+    } catch (error) {
+      alert(setError(error.message));
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+        />
+        <button type="submit">Login</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
+};
 
 export default Login;
--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -1,5 +1,61 @@
+import { useState } from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { collection, addDoc } from 'firebase/firestore';
+import { auth, db } from '../../config/firebase.js';
 function Registration() {
-  return <div>Registration</div>;
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      alert('Signup successful!');
+      const user = userCredential.user;
+      await addDoc(collection(db, 'users'), {
+        uid: user.uid,
+        username: username,
+        email: email,
+      });
+    } catch (error) {
+      alert(setError(error.message));
+    }
+  };
+  return (
+    <div>
+      <h2>Signup</h2>
+      <form onSubmit={handleSignup}>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          placeholder="Username"
+          required
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Email"
+          required
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder="Password"
+          required
+        />
+        <button type="submit">Signup</button>
+      </form>
+      {error && <p>{error}</p>}
+    </div>
+  );
 }
 
 export default Registration;
